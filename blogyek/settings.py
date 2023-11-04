5d5152b377f22a34f72bd3b2670e932fcbc68262
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-11-05 03:25:14 +0500

    Update settings.py

--- blogyek/settings.py
+++ blogyek/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'blogyek.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 # import dj_database_url
 
-DATABASES = {
-    "default": dj_database_url.parse(env("DATABASE_URL"))
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     "default": dj_database_url.parse(env("DATABASE_URL"))
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 CKEDITOR_CONFIGS = {
     'default': {
