4cfbfd2c6445b83e60157472db774f4e3fa4cee0
itparkcebter0071 <109731657+Xusanbek0071@users.noreply.github.com>
2024-07-05 12:40:37 +0500

    Update

--- blogyek/settings.py
+++ blogyek/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "sdlhvjdskljvhpsdvsdvsdv-sdvsd-vsdv45sd4v5ds4v54sdvsdv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
